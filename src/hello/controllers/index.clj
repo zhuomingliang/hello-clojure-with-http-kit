54053e505107e60e44e8edf909e7f211d3e35dab
Jimmy Zhuo <zhuomingliang01@qq.com>
2013-07-11 21:47:08 +0800

    added missing codes

--- src/hello/controllers/index.clj
+++ src/hello/controllers/index.clj
@@ -1,13 +1,13 @@
 (ns hello.controllers.index
   (:use [compojure.core :only [defroutes GET]]
-        [hello.views.comments])
+        [hello.views.snippets.comments])
   (:require [compojure.route :as route]
             [hello.views.index :as index]
             [hello.models.comments :as Comment]))
 
 (defn- render []
-;; (let [comments [{:id 1 :author "James" :message "hello JimmyZ"} {:id 2 :author "JimmyZ" :message "hello James"}]] ;; for testing
-  (let [comments (Comment/find-all)]
+  (let [comments [{:id 1 :author "James" :message "hello JimmyZ"} {:id 2 :author "JimmyZ" :message "aloha James"}]] ;; for testing
+;;(let [comments (Comment/find-all)]
   	(comments-snippet comments)))
 
 (defroutes routes
